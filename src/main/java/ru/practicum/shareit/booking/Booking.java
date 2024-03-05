e2c0e85b6f4024840412f4761a4bd6d4dddf6f6b
Konstantin <tuder.muder@yandex.ru>
2024-03-05 10:01:49 +0400

    fix: исправлены методы equals() и hashCode()

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -51,16 +51,50 @@ public class Booking {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Booking booking = (Booking) o;
+
+        boolean isItemEquals;
+        if (item == null && booking.item == null) {
+            isItemEquals = true;
+        } else {
+            if (item == null || booking.item == null) {
+                isItemEquals = false;
+            } else {
+                isItemEquals = item.getId() == booking.item.getId();
+            }
+        }
+
+        boolean isBookerEquals;
+        if (booker == null && booking.booker == null) {
+            isBookerEquals = true;
+        } else {
+            if (booker == null || booking.booker == null) {
+                isBookerEquals = false;
+            } else {
+                isBookerEquals = booker.getId() == booking.booker.getId();
+            }
+        }
+
         return Objects.equals(id, booking.id)
                 && Objects.equals(start, booking.start)
                 && Objects.equals(end, booking.end)
-                && Objects.equals(item.getId(), booking.item.getId())
-                && Objects.equals(booker.getId(), booking.booker.getId())
+                && isItemEquals
+                && isBookerEquals
                 && status == booking.status;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, start, end, item.getId(), booker.getId(), status);
+        long itemId = 0L;
+        long bookerId = 0L;
+
+        if (item != null) {
+            itemId = item.getId();
+        }
+        if (booker != null) {
+            bookerId = booker.getId();
+        }
+
+
+        return Objects.hash(id, start, end, itemId, bookerId, status);
     }
 }
