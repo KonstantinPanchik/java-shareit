464c3a0a266fbe068f20dfa66a4e33d263f98fe3
Konstantin <tuder.muder@yandex.ru>
2024-03-17 14:05:16 +0400

    fix: убраны пустые строки

--- gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -13,31 +13,25 @@ import java.util.Map;
 @Slf4j
 public class ErrorHandler {
 
-
-
-
     @ExceptionHandler
-    public ResponseEntity handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
-        log.trace("Validation Exception");
-        ResponseEntity access = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        return access;
-    }
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        log.warn("Validation Exception");
 
-
-    @ExceptionHandler
-    public ResponseEntity handleItemNotAvailableException(final IllegalArgumentException e) {
-        log.trace(e.getMessage());
-
-        ResponseEntity access = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", e.getMessage()));
-        return access;
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler
-    public ResponseEntity handleThrowable(final Throwable e) {
-        log.trace(e.getMessage());
-        ResponseEntity ex = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-        return ex;
+    public ResponseEntity<Object> handleItemNotAvailableException(final IllegalArgumentException e) {
+        log.warn(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", e.getMessage()));
     }
 
+    @ExceptionHandler
+    public ResponseEntity<Object> handleThrowable(final Throwable e) {
+        log.warn(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
 
 }
